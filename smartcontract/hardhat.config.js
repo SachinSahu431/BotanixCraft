7615977cf387cd93a39acaa7651a836be43d4cb1
Ganesh-P03 <84033688+Ganesh-P03@users.noreply.github.com>
2024-03-29 16:43:27 +0530

    updated env

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -4,10 +4,10 @@ require("dotenv").config({ path: ".env" });
 const privateKey = process.env.PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: { version: "0.8.20" },
   networks: {
-    opBnB: {
-      url: "https://opbnb-testnet-rpc.bnbchain.org/",
+    testnet: {
+      url: `https://node.botanixlabs.dev`,
       accounts: [`0x${privateKey}`],
     },
   },
